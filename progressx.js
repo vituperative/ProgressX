94cfc12e6915d907951e44f6a08ceeb00ed1fa58
dr|z3d <60452960+vituperative@users.noreply.github.com>
2024-08-24 21:04:07 +0000

    Update progressx.js
    Refactor, remove dead code, add support for custom palettes

--- progressx.js
+++ progressx.js
@@ -1,138 +1,149 @@
-/* ProgressX 1.0, 2018-11-16
+/* ProgressX 1.1, 2024-04-15
  * https://github.com/ryadpasha/progressx
  * Copyright (c) 2018 Ryad Pasha
- * Licensed under the MIT License */
-;(function(window, document) {
-    'use strict'
+ * Licensed under the MIT License
+ * Modifications and optimizations by dr|z3d, 2024 */
 
-    ;(function() {
-        var lastTime = 0;
-        var vendors = ['ms', 'moz', 'webkit', 'o'];
-        for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-            window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-            window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-                || window[vendors[x]+'CancelRequestAnimationFrame'];
-        }
-        if (!window.requestAnimationFrame)
-            window.requestAnimationFrame = function(callback, element) {
-                var currTime = new Date().getTime();
-                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-                var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-                    timeToCall);
-                lastTime = currTime + timeToCall;
-                return id;
-            };
-        if (!window.cancelAnimationFrame)
-            window.cancelAnimationFrame = function(id) {
-                clearTimeout(id);
-            };
-    }());
+function initProgressX(window, document) {
+  "use strict";
 
-    var canvas, progressTimerId, fadeTimerId, currentProgress, showing,
-        addEvent = function(elem, type, handler) {
-            if (elem.addEventListener) elem.addEventListener(type, handler, false)
-            else if (elem.attachEvent) elem.attachEvent('on' + type, handler)
-            else                       elem['on' + type] = handler
-        },
-        options = {
-            autoRun      : true,
-            barThickness : 3,
-            barColors    : {
-                '0'      : 'rgba(26,  188, 156, .9)',
-                '.25'    : 'rgba(52,  152, 219, .9)',
-                '.50'    : 'rgba(241, 196, 15,  .9)',
-                '.75'    : 'rgba(230, 126, 34,  .9)',
-                '1.0'    : 'rgba(211, 84,  0,   .9)'
-            },
-            shadowBlur   : 10,
-            shadowColor  : 'rgba(0,   0,   0,   .6)'
-        },
-        repaint = function() {
-            canvas.width = window.innerWidth
-            canvas.height = options.barThickness * 5 // need space for shadow
+  var lastTime = 0;
 
-            var ctx = canvas.getContext('2d')
-            ctx.shadowBlur = options.shadowBlur
-            ctx.shadowColor = options.shadowColor
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function(callback) {
+      var currTime = Date.now();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function() {callback(currTime + timeToCall);}, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  }
 
-            var lineGradient = ctx.createLinearGradient(0, 0, canvas.width, 0)
-            for (var stop in options.barColors)
-                lineGradient.addColorStop(stop, options.barColors[stop])
-            ctx.lineWidth = options.barThickness
-            ctx.beginPath()
-            ctx.moveTo(0, options.barThickness/2)
-            ctx.lineTo(Math.ceil(currentProgress * canvas.width), options.barThickness/2)
-            ctx.strokeStyle = lineGradient
-            ctx.stroke()
-        },
-        createCanvas = function() {
-            canvas = document.createElement('canvas')
-            var style = canvas.style
-            style.position = 'fixed'
-            style.top = style.left = style.right = style.margin = style.padding = 0
-            style.zIndex = 100001
-            style.display = 'none'
-            document.body.appendChild(canvas)
-            addEvent(window, 'resize', repaint)
-        },
-        progressx = {
-            config: function(opts) {
-                for (var key in opts)
-                    if (options.hasOwnProperty(key))
-                        options[key] = opts[key]
-            },
-            show: function() {
-                if (showing) return
-                showing = true
-                if (fadeTimerId !== null)
-                    window.cancelAnimationFrame(fadeTimerId)
-                if (!canvas) createCanvas()
-                canvas.style.opacity = 1
-                canvas.style.display = 'block'
-                progressx.progress(0)
-                if (options.autoRun) {
-                    (function loop() {
-                        progressTimerId = window.requestAnimationFrame(loop)
-                        progressx.progress('+' + (.05 * Math.pow(1-Math.sqrt(currentProgress), 2)))
-                    })()
-                }
-            },
-            progress: function(to) {
-                if (typeof to === "undefined")
-                    return currentProgress
-                if (typeof to === "string") {
-                    to = (to.indexOf('+') >= 0 || to.indexOf('-') >= 0 ? currentProgress : 0) + parseFloat(to)
-                }
-                currentProgress = to > 1 ? 1 : to
-                repaint()
-                return currentProgress
-            },
-            hide: function() {
-                if (!showing) return
-                showing = false
-                if (progressTimerId != null) {
-                    window.cancelAnimationFrame(progressTimerId)
-                    progressTimerId = null
-                }
-                (function loop() {
-                    if (progressx.progress('+.1') >= 1) {
-                        canvas.style.opacity -= .05
-                        if (canvas.style.opacity <= .05) {
-                            canvas.style.display = 'none'
-                            fadeTimerId = null
-                            return
-                        }
-                    }
-                    fadeTimerId = window.requestAnimationFrame(loop)
-                })()
-            }
-        }
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function(id) {clearTimeout(id);};
+  }
 
-    if (typeof module === 'object' && typeof module.exports === 'object') {
-        module.exports = progressx
-    } else if (typeof define === 'function' && define.amd) {
-        define(function() { return progressx })
-    } else {
-        this.progressx = progressx
+  var canvas, progressTimerId, fadeTimerId, currentProgress, showing, options = {
+    autoRun: true,
+    barThickness: 3,
+    barColors: {
+      default: {
+        "0.0": "rgba(220, 48, 16, .8)",
+        "1.0": "rgba(255, 96, 0, .8)"
+      },
+      classic: {
+        "0.0": "rgba(48, 64, 192, .7)",
+        "1.0": "rgba(48, 64, 255, .7)"
+      },
+      midnight: {
+        "0.0": "rgba(72, 0, 72, .8)",
+        "1.0": "rgba(160, 0, 160, .8)"
+      }
     }
-}).call(this, window, document)
+  };
+
+  function createCanvas() {
+    canvas = document.createElement("canvas");
+    canvas.id = "pageloader";
+    var style = canvas.style;
+    style.position = "fixed";
+    style.top = style.left = style.right = style.margin = style.padding = 0;
+    style.zIndex = 100001;
+    style.display = "none";
+    document.body.appendChild(canvas);
+    window.addEventListener("resize", repaint);
+  }
+
+  function repaint() {
+    var ctx = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = options.barThickness;
+
+    if (options.barColors.current) {
+      var lineGradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+
+      Object.keys(options.barColors.current).forEach(function(stop) {
+        lineGradient.addColorStop(parseFloat(stop), options.barColors.current[stop]);
+      });
+
+      ctx.lineWidth = options.barThickness;
+      ctx.beginPath();
+      ctx.moveTo(0, options.barThickness / 2);
+      ctx.lineTo(Math.ceil(currentProgress * canvas.width), options.barThickness / 2);
+      ctx.strokeStyle = lineGradient;
+      ctx.stroke();
+    }
+  }
+
+  function addEvent(elem, type, handler) {
+    elem.addEventListener(type, handler);
+  }
+
+  function progressxConfig(opts) {
+    for (key in opts) {
+      if (key === "barColors") {
+        var colorSet;
+        for (colorSet in opts[key]) {options.barColors[colorSet] = opts[key][colorSet];}
+      } else {
+        if (options.hasOwnProperty(key)) {options[key] = opts[key];}
+      }
+    }
+  }
+
+  function progressxShow(colorSet) {
+    if (showing) {return;}
+    showing = true;
+    if (fadeTimerId !== null) {window.cancelAnimationFrame(fadeTimerId);}
+    if (!canvas) {createCanvas();}
+    canvas.style.opacity = 1;
+    canvas.style.display = "block";
+    progressxProgress(0);
+    options.barColors.current = options.barColors[colorSet] || options.barColors.default;
+    if (options.autoRun) {
+      (function loop() {
+        progressTimerId = window.requestAnimationFrame(loop);
+        progressxProgress("+" + (0.05 * Math.pow(1 - Math.sqrt(currentProgress), 2)));
+      })();
+    }
+  }
+
+  function progressxProgress(to) {
+    if (typeof to === "string") {
+      to = (to.indexOf("+") >= 0 || to.indexOf("-") >= 0 ? currentProgress : 0) + parseFloat(to);
+    }
+    currentProgress = to > 1 ? 1 : to;
+    repaint();
+    return currentProgress;
+  }
+
+  function progressxHide() {
+    if (!showing) {return;}
+    showing = false;
+    if (progressTimerId != null) {
+      window.cancelAnimationFrame(progressTimerId);
+      progressTimerId = null;
+    }
+    (function loop() {
+      if (progressxProgress("+.1") >= 1) {
+        canvas.style.opacity -= 0.25;
+        if (canvas.style.opacity <= 0.25) {
+          canvas.style.display = "none";
+          fadeTimerId = null;
+          return;
+        }
+      }
+      fadeTimerId = window.requestAnimationFrame(loop);
+    })();
+  }
+
+  if (typeof module === "object" && typeof module.exports === "object") {
+    module.exports = { config: progressxConfig, show: progressxShow, progress: progressxProgress, hide: progressxHide };
+  } else if (typeof define === "function" && define.amd) {
+    define(function() {
+      return { config: progressxConfig, show: progressxShow, progress: progressxProgress, hide: progressxHide };
+    });
+  } else {
+    window.progressx = { config: progressxConfig, show: progressxShow, progress: progressxProgress, hide: progressxHide };
+  }
+}
+initProgressX(window, document);
